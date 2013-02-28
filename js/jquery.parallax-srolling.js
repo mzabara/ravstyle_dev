30747102f6c54e67206624d790d8f3eeee4c9dce
Michael B <webmikez@gmail.com>
2013-02-28 11:43:53 +0700

    Добавлена анимация для паралакса, улучшение поведения

--- js/jquery.parallax-srolling.js
+++ js/jquery.parallax-srolling.js
@@ -8,44 +8,6 @@
  * 		   @richardshepherd
  */
 
-
-function setupParallax() {
-    $window = $(window);
-    $("[data-type]").each(function () {
-        $(this).data("offsetY", parseInt($(this).attr("data-offsetY")));
-        $(this).data("Xposition", $(this).attr("data-Xposition"));
-        $(this).data("speed", $(this).attr("data-speed"))
-    });
-    $('section[data-type="background"]').each(function () {
-        var a = $(this), b = a.offset(), c = b.top;
-        $(window).scroll(function () {
-            if ($window.scrollTop() + $window.height() > c && c + a.height() > $window.scrollTop()) {
-                var b = -($window.scrollTop() / a.data("speed"));
-                if (a.data("offsetY")) {
-                    b += a.data("offsetY")
-                }
-                var d = "(50% " + b + "px)";
-                a.stop().animate({backgroundPosition:d}, 1500, "easeOutExpo");
-                $('[data-type="background_sprite"]', a).each(function () {
-                    var a = $(this);
-                    var b = -($window.scrollTop() / a.data("speed"));
-                    var c = a.data("Xposition") + " " + (b + a.data("offsetY")) + "px";
-                    a.stop().animate({backgroundPosition:c}, 1500, "easeOutQuart", function () {
-                    })
-                });
-                $('[data-type="element"]', a).each(function () {
-                    var a = $(this);
-                    var b = -($window.scrollTop() / a.data("speed"));
-                    var c = b + a.data("offsetY") + "px";
-                    a.stop().animate({top:c}, 1500, "easeOutExpo", function () {
-                    })
-                })
-            }
-        })
-    })
-}
-
-
 // On your marks, get set...
 $(document).ready(function(){
 
@@ -88,8 +50,7 @@ $(document).ready(function(){
                 var coords = '50% '+ yPos + 'px';
 
                 // Move the background
-                $self.css({ backgroundPosition: coords });
-                $self.css({ backgroundPosition: coords });
+                $self.stop().animate({backgroundPosition: coords}, 1500, "easeOutExpo");
 
                 // Check for other sprites in this section
                 $('[data-type="sprite"]', $self).each(function() {
@@ -101,7 +62,7 @@ $(document).ready(function(){
                     var yPos = -($window.scrollTop() / $sprite.data('speed'));
                     var coords = $sprite.data('Xposition') + ' ' + (yPos + $sprite.data('offsetY')) + 'px';
 
-                    $sprite.css({ backgroundPosition: coords });
+                    $sprite.stop().animate({backgroundPosition: coords}, 1500, "easeOutQuart", function () {});
 
                 }); // sprites
 
@@ -121,18 +82,11 @@ $(document).ready(function(){
                 }); // video
 
                 $('[data-type="element"]', $self).each(function() {
-
-                    // Cache the video
-                    var $video = $(this);
-
-                    // There's some repetition going on here, so
-                    // feel free to tidy this section up.
-                    var yPos = -($window.scrollTop() / $video.data('speed'));
-                    var coords = (yPos + $video.data('offsetY')) + 'px';
-
-                    $video.css({ top: coords });
-
-                }); // video
+                    var a = $(this);
+                    var yPos = -($window.scrollTop() / a.data("speed"));
+                    var coords = yPos + a.data("offsetY") + "px";
+                    a.stop().animate({top: coords}, 1500, "easeOutExpo");
+                }); // element
 
             }; // in view
 
