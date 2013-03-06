27d451f3910b5c89a60e8c9fbda9aea0b9a52b68
Michael B <webmikez@gmail.com>
2013-03-06 12:31:05 +0700

    Исправлена анимация фона

--- ver_2/js/function.js
+++ ver_2/js/function.js
@@ -211,3 +211,41 @@ $(window).load(function (e) {
     setupEventHandlers();
 });
 
+function setupParallax() {
+    $window = $("#wrapper");
+    $("[data-type]").each(function () {
+        $(this).data("offsetY", parseInt($(this).attr("data-offsetY")));
+        $(this).data("Xposition", $(this).attr("data-Xposition"));
+        $(this).data("speed", $(this).attr("data-speed"))
+    });
+
+    $('section[data-type="background"]').each(function () {
+        var a = $(this), b = a.offset(), c = b.top;
+        $("#wrapper").scroll(function () {
+            if ($window.scrollTop() + $window.height() > c && c + a.height() > $window.scrollTop()) {
+                var b = -($window.scrollTop() / a.data("speed"));
+                if (a.data("offsetY")) {
+                    b += a.data("offsetY")
+                }
+                var dx = '50%';
+                var dy = b + 'px';
+                a.animate({'background-position-x': dx, 'background-position-y': dy}, 1500, "easeOutExpo");
+                $('[data-type="background_sprite"]', a).each(function () {
+                    var a = $(this);
+                    var b = -($window.scrollTop() / a.data("speed"));
+                    var c = a.data("Xposition") + " " + (b + a.data("offsetY")) + "px";
+                    a.stop().animate({backgroundPosition:c}, 1500, "easeOutQuart", function () {
+                    })
+                });
+                $('[data-type="element"]', a).each(function () {
+                    var a = $(this);
+                    var b = -($window.scrollTop() / a.data("speed"));
+                    var c = b + a.data("offsetY") + "px";
+                    a.stop().animate({top:c}, 1500, "easeOutExpo", function () {
+                    })
+                })
+            }
+        })
+    })
+}
+
